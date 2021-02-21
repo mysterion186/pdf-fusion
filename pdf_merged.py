58b06a75858d6a7889db8d7419f352e7dbd13896
Julien Ravindrarasa <kowsikan@MacBook-Pro-de-Julien.local>
2021-02-21 11:11:08 +0100

    test update

--- pdf_merged.py
+++ pdf_merged.py
@@ -3,16 +3,14 @@ from PyPDF2 import PdfFileMerger
 #from PyPDF2 import PdfFileReader
 import os
 
-
+test = 'Ceci est un test pour voir le nouveau fichier git 2'
 
 list_files=os.listdir("fusion/")   #on obtient une liste de str contenant l'ensemble des fichier/dossier du répertoire (même des fichier non pdf)
 
 nom_fichier =  input("Nom du pdf fusionné ")
 
 #on crée une liste contenant QUE les noms des fichier pdf
-"""
-be sure that there is only pdf files in the directory
-"""
+
 pdfs = []
 
 for file in list_files:
